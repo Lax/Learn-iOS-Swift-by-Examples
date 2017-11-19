84042880ef07ca6cd4538f862171dd528ae903dd
Liu Lantao <liulantao@gmail.com>
2017-11-20 07:18:57 +0800

    add update date
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

--- .scripts/fetch-samplecode.sh
+++ .scripts/fetch-samplecode.sh
@@ -4,11 +4,11 @@ curl 'https://developer.apple.com/search/search_data.php?q=swift%203&results=500
 
 ruby -e "require 'json';
 require 'csv';
-CSV.open('_projects.csv', 'wb') do |csv|
+CSV.open('samplecode.csv', 'wb') do |csv|
   JSON.parse(File.open('/tmp/search_result.json').read)['results'].keep_if {|v|
     v['type'] == 'sample_code'
   }.sort_by {|x| x['title']}.each{|sc|
-    csv << [sc['url'].match('\/samplecode\/([^\/]*)\/')[1], 'https://developer.apple.com' + sc['url'] + 'Introduction/Intro.html', sc['title']]
+    csv << [sc['url'].match('\/samplecode\/([^\/]*)\/')[1], 'https://developer.apple.com' + sc['url'] + 'Introduction/Intro.html', sc['title'], sc['date']]
   }
 end
 "
